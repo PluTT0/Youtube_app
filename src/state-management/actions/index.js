6f93c80151159d2025c9e8f713bf2b8454c74b7a
Dmytro Kos <kos.sok9205@gmail.com>
2021-08-26 21:00:52 +0200

    Update index.js

--- src/state-management/actions/index.js
+++ src/state-management/actions/index.js
@@ -1,9 +1,8 @@
 import { getYoutubeData } from './asyncAction/getData';
-import { setIsFetching, setError, setFetchResult } from './actionCreaters';
+import { setIsFetching, setFetchResult } from './actionCreaters';
 
 export {
   getYoutubeData,
   setIsFetching,
-  setError,
   setFetchResult
 }
